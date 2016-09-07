c83fd468887a93ce5c42b92f568868ade8dfa67a
aj1310 <aj_ortiz1310@icloud.com>
2016-09-07 07:55:40 -0300

    Fix icon switch

--- src/components/RoutesMap.js
+++ src/components/RoutesMap.js
@@ -17,15 +17,15 @@ const RoutesMap = ({ selectedRoute, onRouteMarkerClick }) => (
               {selectedRoute.stops.map((stop, index) => {
                 return (
                   <GoogleMapMarker key={stop.id} position={{ lat: stop.location.latitude, lng: stop.location.longitude }} label={String(index + 1)} icon={{
-                    path: 'M32 18l-4-8h-6v-4c0-1.1-0.9-2-2-2h-18c-1.1 0-2 0.9-2 2v16l2 2h2.536c-0.341 0.588-0.536 1.271-0.536 2 0 2.209 1.791 4 4 4s4-1.791 4-4c0-0.729-0.196-1.412-0.536-2h11.073c-0.341 0.588-0.537 1.271-0.537 2 0 2.209 1.791 4 4 4s4-1.791 4-4c0-0.729-0.196-1.412-0.537-2h2.537v-6zM22 18v-6h4.146l3 6h-7.146z',
-                    fillColor: 'hsl(168, 75.4%, 42.2%)',
+                    path: 'M16 0c-5.523 0-10 4.477-10 10 0 10 10 22 10 22s10-12 10-22c0-5.523-4.477-10-10-10zM16 16.125c-3.383 0-6.125-2.742-6.125-6.125s2.742-6.125 6.125-6.125 6.125 2.742 6.125 6.125-2.742 6.125-6.125 6.125zM12.125 10c0-2.14 1.735-3.875 3.875-3.875s3.875 1.735 3.875 3.875c0 2.14-1.735 3.875-3.875 3.875s-3.875-1.735-3.875-3.875z',
+                    fillColor: 'hsl(48, 100%, 50.5%)',
                     fillOpacity: 0.8,
                   }} map={map} onClick={onRouteMarkerClick} />
                 );
               })}
               <GoogleMapMarker key={selectedRoute.driver.id} position={{ lat: selectedRoute.driver.location.latitude, lng: selectedRoute.driver.location.longitude }} title={selectedRoute.driver.name} icon={{
-                path: 'M16 0c-5.523 0-10 4.477-10 10 0 10 10 22 10 22s10-12 10-22c0-5.523-4.477-10-10-10zM16 16.125c-3.383 0-6.125-2.742-6.125-6.125s2.742-6.125 6.125-6.125 6.125 2.742 6.125 6.125-2.742 6.125-6.125 6.125zM12.125 10c0-2.14 1.735-3.875 3.875-3.875s3.875 1.735 3.875 3.875c0 2.14-1.735 3.875-3.875 3.875s-3.875-1.735-3.875-3.875z',
-                fillColor: 'hsl(48, 100%, 50.5%)',
+                path: 'M32 18l-4-8h-6v-4c0-1.1-0.9-2-2-2h-18c-1.1 0-2 0.9-2 2v16l2 2h2.536c-0.341 0.588-0.536 1.271-0.536 2 0 2.209 1.791 4 4 4s4-1.791 4-4c0-0.729-0.196-1.412-0.536-2h11.073c-0.341 0.588-0.537 1.271-0.537 2 0 2.209 1.791 4 4 4s4-1.791 4-4c0-0.729-0.196-1.412-0.537-2h2.537v-6zM22 18v-6h4.146l3 6h-7.146z',
+                fillColor: 'hsl(168, 75.4%, 42.2%)',
                 fillOpacity: 0.8,
               }} map={map} onClick={onRouteMarkerClick} />
               {selectedRoute.tour &&
